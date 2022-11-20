55ede94a1b54d829ba8fe5370d469f63313071c5
Ryan Jin <ryanjin333@gmail.com>
2022-11-20 15:25:23 -0800

    minor updates

--- functions/index.js
+++ functions/index.js
@@ -24,44 +24,11 @@ holidaysQuery.onSnapshot( (querySnapshot) => {
   console.log(`Encountered error: ${error}`);
 })
 
-//Update isFirstDay
-
-var isHoliday = false;
-var endDate = "";
-
-const changeDay = async () => {
-  if (moment().format('H') === '12' //&& 
-    //(moment().format('ddd') !== 'Sat' || moment().format('ddd') !== 'Sun')
-    ) {
-      console.log("Day Change");
-    //Call if the date is not a holiday  
-    if (!isHoliday) {
-      holidays.forEach((holiday) => {
-        if (moment().format('L') === holiday.startDate) {
-          isHoliday = true;
-          endDate = holiday.endDate;
-        }
-      })
-    }
-    
-    //Write to database if the date isn't a holiday or if an end date has been reached
-    if (!isHoliday || moment().format('L') === endDate) {
-      await db.collection("booleans").doc("isFirstDay").set({
-        "isFirstDay" : !isFirstDay
-      })
-
-      isHoliday = false;
-      endDate = "";
-    }
-  }
-}
-setInterval(changeDay, 1000);
-
 //API GET request
-    app.get('/isFirstDay', (req, res) => {
-        res.status(200).send({
-            isFirstDay : isFirstDay
-        })
+app.get('/isFirstDay', (req, res) => {
+    res.status(200).send({
+        isFirstDay : isFirstDay
     })
+})
 
 exports.app = functions.https.onRequest(app);
