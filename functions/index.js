d4a33429e30e9ef5de084e2bc47ab2e8a3acb258
Ryan Jin <ryanjin333@gmail.com>
2022-11-25 14:58:42 -0800

    v1 complete

--- functions/index.js
+++ functions/index.js
@@ -24,11 +24,47 @@ holidaysQuery.onSnapshot( (querySnapshot) => {
   console.log(`Encountered error: ${error}`);
 })
 
+//Update isFirstDay
+var isHoliday = false;
+var endDate = "";
+
+const changeDay = async () => {
+  if (moment().format('H') === '0' && 
+    (moment().format('ddd') !== 'Sat' || moment().format('ddd') !== 'Sun')
+    ) {
+    //Call if the date is not a holiday  
+    if (!isHoliday) {
+      holidays.forEach((holiday) => {
+        if (moment().format('L') === holiday.startDate) {
+          isHoliday = true;
+          endDate = holiday.endDate;
+        }
+      })
+    }
+    
+    //Write to database if the date isn't a holiday or if an end date has been reached
+    if (!isHoliday || moment().format('L') === endDate) {
+      await db.collection("booleans").doc("isFirstDay").set({
+        "isFirstDay" : !isFirstDay
+      })
+
+      isHoliday = false;
+      endDate = "";
+    }
+  }
+}
+setInterval(changeDay, 3600000);
+
+
 //API GET request
 app.get('/isFirstDay', (req, res) => {
     res.status(200).send({
         isFirstDay : isFirstDay
     })
 })
+
+exports.updateDay = functions.firestore
+  .document('booleans/isFirstDay')
+  .onUpdate()
 
 exports.app = functions.https.onRequest(app);
